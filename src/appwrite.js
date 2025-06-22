d2e7c10bffc9464ce5bb26f73862673e369b2847
Vedant Shimpi <vedantshimpi@Vedants-MacBook-Air.local>
2025-06-22 13:06:29 +0530

    Initial commit

---
+++ src/appwrite.js
@@ -0,0 +1,57 @@
+import { Client, Databases, ID, Query } from 'appwrite'
+
+const PROJECT_ID = import.meta.env.VITE_APPWRITE_PROJECT_ID;
+const DATABASE_ID = import.meta.env.VITE_APPWRITE_DATABASE_ID;
+const COLLECTION_ID = import.meta.env.VITE_APPWRITE_COLLECTION_ID;
+
+const client = new Client()
+  .setEndpoint('https://nyc.cloud.appwrite.io/v1')
+  .setProject(PROJECT_ID)
+
+const database = new Databases(client);
+
+export const updateSearchCount = async (searchTerm, movie) => {
+  try {
+    console.log("Updating search count for:", searchTerm, movie);
+
+    const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+      Query.equal('searchTerm', searchTerm),
+    ]);
+
+    console.log("Search result:", result);
+
+    if (result.documents.length > 0) {
+      const doc = result.documents[0];
+      await database.updateDocument(DATABASE_ID, COLLECTION_ID, doc.$id, {
+        count: doc.count + 1,
+      });
+      console.log("Updated document:", doc.$id);
+    } else {
+      const newDoc = await database.createDocument(DATABASE_ID, COLLECTION_ID, ID.unique(), {
+        searchTerm,
+        count: 1,
+        movie_ID: movie.id,
+        poster_URL: movie.poster_path
+          ? `https://image.tmdb.org/t/p/w500${movie.poster_path}`
+          : 'https://via.placeholder.com/150',
+      });
+      console.log("Created new document:", newDoc);
+    }
+  } catch (error) {
+    console.error("Error in updateSearchCount:", error);
+  }
+};
+
+
+export const getTrendingMovies = async () => {
+ try {
+  const result = await database.listDocuments(DATABASE_ID, COLLECTION_ID, [
+    Query.limit(5),
+    Query.orderDesc("count")
+  ])
+
+  return result.documents;
+ } catch (error) {
+  console.error(error);
+ }
+}
